2503a7fd0174ce9185c6a4f05b081e6e04e35daa
jammanjones <vslsphoto@gmail.com>
2026-07-11 03:20:05 +0000

    Version 0.5.1 - Added wheel spin animation

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const wheel = document.getElementById("wheel");
+const spinButton = document.getElementById("spinButton");
 
+let currentRotation = 0;
+
+spinButton.addEventListener("click", spinWheel);
+
+function spinWheel(){
+
+    spinButton.disabled = true;
+
+    let randomSpin = Math.floor(Math.random() * 360) + 1440;
+
+    currentRotation += randomSpin;
+
+    wheel.style.transition = "transform 4s ease-out";
+
+    wheel.style.transform = 
+    `rotate(${currentRotation}deg)`;
+
+
+    setTimeout(() => {
+
+        spinButton.disabled = false;
+
+    },4000);
+
+}
